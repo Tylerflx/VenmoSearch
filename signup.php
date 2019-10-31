ae77429774a1d8cbf2a0c72e5e87dcb4d13f917d
Tyler <ruafino17@gmail.com>
2019-10-30 23:37:50 -0400

    init

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Sign Up </title>
+</head>
+<body>
+<div class="header">
+    <h1> Welcome to VenmoSearch </h1>
+
+</div>
+
+
+<form>
+    <table>
+        <tr>
+            <td>Username:</td>
+            <td>input type="text" name="username" class="textInput"></td>
+        </tr>
+    </table>
+</form>
+
+
+
+
+</body>
+</html>
